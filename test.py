6866c76be9bae4234b9ef3e5d6957aad9f18decc
Sergey Karatach <karatach1998@yandex.ru>
2018-12-15 02:23:23 +0300

    Add test_insert

--- test.py
+++ test.py
@@ -10,6 +10,21 @@ class TestBTree(unittest.TestCase):
         self.assertEqual(t.root.leaf, True)
         self.assertEqual(t.root.n, 0)
 
+    def _compare_btree(t, s):
+        leaf, n, keys, children = s
+        assertEqual(t.leaf, leaf)
+        assertEqual(t.n, n)
+        assertEqual(t.keys, keys)
+        for t, s in zip(t.children, children):
+            _compare_btree(t, s)
+
+    def test_insert(self):
+        t = BTree()
+        for x in [5, 1, 4, 2, 3, 3]:
+            t.insert(x)
+        self.assertEqual(t.root.leaf, False)
+        expected_structure = (False, [], [])
+
 
 if __name__ == '__main__':
     unittest.main()
