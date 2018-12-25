c4bd87b8534deb6b835fcc72dd14dc6b5d408e36
Sergey Karatach <karatach1998@yandex.ru>
2018-12-25 20:25:11 +0300

    Add more test_delete_* cases

--- test.py
+++ test.py
@@ -39,18 +39,56 @@ class TestBTree(unittest.TestCase):
             t.insert(x)
         self.assertEqual(t.search(3), (Node(keys=[3, 3], children=[]), 0))
 
-    def test_delete(self):
+    def test_delete_last_in_root_leaf(self):
         t = BTree()
-        for x in [5, 1, 4, 2, 3, 3]:
+        t.insert(5)
+        t.delete(5)
+        expected_structure = (True, 0, [], [])
+        self._assert_btree(t.root, expected_structure)
+
+    def test_delete_from_empty_tree(self):
+        t = BTree()
+        t.delete(1)
+        expected_structure = (True, 0, [], [])
+        self._assert_btree(t.root, expected_structure)
+
+    def test_delete_last_in_leaf_to_force_tree_shrink_in_height(self):
+        t = BTree()
+        for x in [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]:
             t.insert(x)
-        t.delete(3)
-        expected_structure = (False, 2, [2, 4], [
-            (True, 1, [1], []),
-            (True, 1, [3], []),
-            (True, 1, [5], [])
+        t.delete(2)
+        t.delete(1)
+        expected_structure = (False, 1, [7], [
+            (False, 1, [5], [
+                (True, 2, [3, 4], []),
+                (True, 1, [6], []),
+            ]),
+            (False, 1, [9], [
+                (True, 1, [8], []),
+                (True, 1, [10], [])
+            ])
         ])
         self._assert_btree(t.root, expected_structure)
 
+    def test_delete_not_existing(self):
+        t = BTree()
+        for x in [1, 2, 3, 4]:
+            t.insert(x)
+        t.delete(5)
+        expected_structure = (False, 1, [2], [
+            (True, 1, [1], []),
+            (True, 2, [3, 4], [])
+        ])
+        self._assert_btree(t.root, expected_structure)
+
+    def test_delete_all(self):
+        t = BTree()
+        for x in [6, 1, 4, 5, 2, 3, 3]:
+            t.insert(x)
+        for x in [1, 2, 3, 3, 4, 5, 6]:
+            t.delete(x)
+        expected_structure = (True, 0, [], [])
+        self._assert_btree(t.root, expected_structure)
 
 
 if __name__ == '__main__':
